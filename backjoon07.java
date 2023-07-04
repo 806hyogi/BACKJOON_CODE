3b29b5dfa46a1d4334076946e5491a72b97697d1
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-07-04 21:15:23 +0900

    자바 day02

---
+++ backjoon07.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.Scanner;
+
+
+public class backjoon07 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String inputString;
+		
+		inputString = sc.nextLine(); // 입력값 받음
+		
+		System.out.println(inputString + "??!"); // 출력값
+	}
+
+}

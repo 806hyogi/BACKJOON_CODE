e3575c9f736f3bfa6b57ab586e3de9d12d28d3bb
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-07-06 22:25:11 +0900

    자바 day04

---
+++ backjoon09.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Scanner;
+
+public class backjoon07 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		
+		int num = Integer.parseInt(input);
+		
+		int result = num - 543;
+		
+		System.out.println(result);
+		
+	}
+
+}

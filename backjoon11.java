167e93928d7d13da5daeae6e5929b7fbc0745531
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-07-07 17:31:49 +0900

    자바 day05

--- backjoon11.java
+++ backjoon11.java
@@ -2,24 +2,24 @@ package test;
 
 import java.util.Scanner;
 
-public class backjoon10 {
+public class backjoon11 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		int strNum = Integer.parseInt(str);
 		
-		for(int i=1; i<=strNum; i++) {
-			
-			String num1 = sc.next();
-			String num2 = sc.next();
-			
-			int number1 = Integer.parseInt(num1);
-			int number2 = Integer.parseInt(num2);
-			
-			
-			System.out.println("Case #" + i + ":" + " " + number1 + " + " + number2 + " = " + (number1+number2));
+		Scanner sc = new Scanner(System.in);
+		String inputData = sc.nextLine();
+		int Data = Integer.parseInt(inputData);
+		
+		for(int i = 0; i < Data; i++) {
+
+			for(int j = 0; j <= i*1; j++) {
+				
+				System.out.print("*");
+				
+			}
+			System.out.println("");
 		}
+
 	}
 
 }

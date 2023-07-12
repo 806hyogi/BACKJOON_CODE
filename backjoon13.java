b16a9da97b8fc4ea26c929df9ce4421c202c69f8
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-07-12 09:30:18 +0900

    자바 day07

---
+++ backjoon13.java
@@ -0,0 +1,25 @@
+package Test;
+import java.util.Scanner;
+public class backjoon13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		boolean a = true;
+		
+		while(a) {
+			int inputdata = sc.nextInt();
+			int inputdata2 = sc.nextInt();
+			
+			int sum = inputdata + inputdata2;
+			
+			
+			System.out.println(sum);
+			
+			
+		}
+	}
+
+}
